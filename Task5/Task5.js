148e8fe7ba584a589261a9958eecbdf045788f21
shahed <shahedibrahim7@gmail.com>
2025-01-24 00:45:37 +0300

    Added Task5 Folder

---
+++ Task5/Task5.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+function createAndSum() {
+    let numbers = new Array(101);
+    let sum = 0;
+
+    for (let i = 0; i <= 100; i++) {
+        numbers[i] = i;
+        if (i % 2 === 0)
+            sum += i;
+    }
+    document.getElementById("displaySum").textContent = "Sum: " + sum;
+    document.getElementById("clicked").style.display = "none";
+}
